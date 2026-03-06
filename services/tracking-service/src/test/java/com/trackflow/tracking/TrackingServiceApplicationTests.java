695aaace2bd3d9a36e6b901411e65dba177fd5b9
marco.domingues <marco.a.dom78@gmail.com>
2026-03-06 20:56:25 +0000

    Initialize TrackFlow platform with microservices workspace

---
+++ services/tracking-service/src/test/java/com/trackflow/tracking/TrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackflow.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
